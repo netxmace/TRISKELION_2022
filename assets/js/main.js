f3b5e5afcb9a461d5c88425f766c2d0a26c1c4cb
Dheeraj Nalapat <dheerajnalapat3@gmail.com>
2022-03-08 22:47:18 +0530

    first change

--- assets/js/main.js
+++ assets/js/main.js
@@ -226,3 +226,26 @@
   });
 
 })()
+
+var countDownDate = new Date("March 1, 2022 00:00:00").getTime();
+
+var x = setInterval(function() {
+
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    var hours = Math.floor((distance)/( 1000 * 60 * 60 ));
+    var minutes = Math.floor((distance%( 1000 * 60 * 60 ))/( 1000 * 60 ));
+    var seconds = Math.floor((distance%( 1000 * 60 ))/( 1000 ));
+
+    document.getElementById("countdown1").innerHTML = hours  ;
+    document.getElementById("countdown2").innerHTML =  minutes ;
+    document.getElementById("countdown3").innerHTML = seconds ;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "TIME OUT";
+    }
+
+},1000);
