d59e1c2192dc9cd658b5ec5bc7ce126ace9fe6e2
Dheeraj Nalapat <dheerajnalapat3@gmail.com>
2022-03-09 18:23:15 +0530

    try2

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,22 +1,47 @@
-var countDownDate = new Date("March 1, 2022 00:00:00").getTime();
+// var countDownDate = new Date("March 1, 2022 00:00:00").getTime();
 
-var x = setInterval(function() {
+// var x = setInterval(function() {
 
-    var now = new Date().getTime();
+//     var now = new Date().getTime();
 
-    var distance = countDownDate - now;
+//     var distance = countDownDate - now;
 
-    var hours = Math.floor((distance)/( 1000 * 60 * 60 ));
-    var minutes = Math.floor((distance%( 1000 * 60 * 60 ))/( 1000 * 60 ));
-    var seconds = Math.floor((distance%( 1000 * 60 ))/( 1000 ));
+//     var hours = Math.floor((distance)/( 1000 * 60 * 60 ));
+//     var minutes = Math.floor((distance%( 1000 * 60 * 60 ))/( 1000 * 60 ));
+//     var seconds = Math.floor((distance%( 1000 * 60 ))/( 1000 ));
 
-    document.getElementById("countdown1").innerHTML = hours  ;
-    document.getElementById("countdown2").innerHTML =  minutes ;
-    document.getElementById("countdown3").innerHTML = seconds ;
+//     document.getElementById("countdown1").innerHTML = hours  ;
+//     document.getElementById("countdown2").innerHTML =  minutes ;
+//     document.getElementById("countdown3").innerHTML = seconds ;
 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("countdown").innerHTML = "TIME OUT";
-    }
+//     if (distance < 0) {
+//         clearInterval(x);
+//         document.getElementById("countdown").innerHTML = "TIME OUT";
+//     }
 
-},1000);
+// },1000);
+
+
+function updateTimer() {
+    future = Date.parse("sept 2, 2020 11:30:00");
+    now = new Date();
+    diff = future - now;
+
+    days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    hours = Math.floor(diff / (1000 * 60 * 60));
+    mins = Math.floor(diff / (1000 * 60));
+    secs = Math.floor(diff / 1000);
+
+    d = days;
+    h = hours - days * 24;
+    m = mins - hours * 60;
+    s = secs - mins * 60;
+
+    document.getElementById("timer")
+        .innerHTML =
+        '<div>' + d + '<span>days</span></div>' +
+        '<div>' + h + '<span>hours</span></div>' +
+        '<div>' + m + '<span>minutes</span></div>' +
+        '<div>' + s + '<span>seconds</span></div>';
+}
+setInterval('updateTimer()', 1000);
